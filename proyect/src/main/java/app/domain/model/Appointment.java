131f22e534be72e57aa9390b2d3a4da84e2ff386
Camila Gómez <camii7312@gmail.com>
2025-09-20 20:17:18 -0500

    Creacion de la clase visita y cita

---
+++ proyect/src/main/java/app/domain/model/Appointment.java
@@ -0,0 +1,79 @@
+package app.domain.model;
+
+import java.util.Date;
+
+
+public class Appointment {
+    private int appointmentId;
+    private Date date;
+    private String time;
+    private String reason;
+    private String status;
+    private Patient patient;
+    private int doctorId;
+    private String appointmentType;
+
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+
+    public void setAppointmentId(int appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getAppointmentType() {
+        return appointmentType;
+    }
+
+    public void setAppointmentType(String appointmentType) {
+        this.appointmentType = appointmentType;
+    }
+}
---
+++ proyect/src/main/java/app/domain/model/Visit.java
@@ -0,0 +1,52 @@
+package app.domain.model;
+
+import java.util.Date;
+
+
+public class Visit {
+    private int visitId;
+    private Date visitDate;
+    private String startTime;
+    private String endTime;
+    private String notes;
+
+    public int getVisitId() {
+        return visitId;
+    }
+
+    public void setVisitId(int visitId) {
+        this.visitId = visitId;
+    }
+
+    public Date getVisitDate() {
+        return visitDate;
+    }
+
+    public void setVisitDate(Date visitDate) {
+        this.visitDate = visitDate;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
